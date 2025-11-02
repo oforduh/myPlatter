db8e779629999c784be3309640f3151ef88c8baa
oforduh <activation340@gmail.com>
2025-11-02 18:35:58 +0100

    edited the nav links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
               { href: "/", label: "Home" },
               { href: "/about", label: "About" },
               { href: "/blog", label: "Blog" },
-              { href: "/plattar-fest", label: "Platter Fest" },
+              { href: "/plattar-fest", label: "Plattar Fest" },
             ].map((link) => (
               <Link
                 key={link.href}
